463aa33f52e7bbd357cc4d9a5ce0fb611337b1dc
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-08-07 07:46:12 +0000

    [Add]devise_public

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root :to => "public/homes#top"
 
   scope module: :public do
+    devise_for :users, :controllers =>{
+      :registrations => 'public/users/registrations',
+      :sessions => 'public/users/sessions'
+    }
   end
 
   namespace :admin do
