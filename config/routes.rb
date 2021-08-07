e16f79937a3208538bfb2ae13d9545521e1104a3
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-08-07 08:49:17 +0000

    [Add]devise_admin

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,11 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root :to => "homes#top"
+    
   end
+  
+  devise_for :admin , controllers: {
+    sessions: 'admin/admins/sessions'
+  }
+  
 end
