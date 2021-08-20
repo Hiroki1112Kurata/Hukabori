1032fcc626cd62ed8e2bdddbc055610673929f50
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-08-20 15:33:45 +0000

    [Add]css-report_index_public

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,17 +1,15 @@
 class Public::BookmarksController < ApplicationController
 
   def create
-    report = Report.find(params[:report_id])
-    bookmark = current_user.bookmarks.new(report_id: report.id)
+    @report = Report.find(params[:report_id])
+    bookmark = current_user.bookmarks.new(report_id: @report.id)
     bookmark.save
-    redirect_to report_path(report)
   end
 
   def destroy
-    report = Report.find(params[:report_id])
-    bookmark = current_user.bookmarks.find_by(report_id: report.id)
+    @report = Report.find(params[:report_id])
+    bookmark = current_user.bookmarks.find_by(report_id: @report.id)
     bookmark.destroy
-    redirect_to report_path(report)
   end
 
 end
