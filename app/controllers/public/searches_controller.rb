a864d844a1bd0f8f3c325554827d9faf66fed629
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-08-29 02:42:56 +0000

    [Add]tag_rearch

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,26 +1,41 @@
 class Public::SearchesController < ApplicationController
   def search
-    @value = params["search"]["value"]
+    case params[:order_sort]
+      # タグ検索
+      when "0"
+          @tag_list = Tag.find(params[:id])
+          @datas = @tag_list.reports.publiced.page(params[:page]).per(6)
 
-    if params[:id].present?
-      @tag_list = Tag.find(params[:id])
-      @report = @tag_list.reports.all
-      @datas = search1_for(@value, @report)
-    else
-      @datas = search2_for(@value)
+      # 検索フォーム
+      when "1"
+        @value = params["search"]["value"]
+
+        # 1_タグ＆キーワード検索
+        if params[:id].present?
+          @tag_list = Tag.find(params[:id])
+          @report = @tag_list.reports.all
+          @datas = search1_for(@value, @report)
+
+        # 2_キーワード検索
+        else
+          @datas = search2_for(@value)
+        end
+
     end
+
+
   end
 
 
   private
 
+  # 1_タグ＆キーワード検索
   def search1_for(value, report)
     Kaminari.paginate_array(report.publiced.where("title LIKE ?", "%#{value}%")).page(params[:page]).per(6)
-    # 部分一致　検索
   end
 
+  # 2_キーワード検索
   def search2_for(value)
     Kaminari.paginate_array(Report.publiced.where("title LIKE ?", "%#{value}%")).page(params[:page]).per(6)
-    # 部分一致　検索
   end
 end
